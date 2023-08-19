01e3ba710489c3268d7e80e14456231e1a6dabe7
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-08-19 13:19:49 -0500

    fix - Party spawn location

--- Assets/Managers/PartyManager.cs
+++ Assets/Managers/PartyManager.cs
@@ -27,9 +27,10 @@ public class PartyManager : MonoBehaviour
 
     private void TestParty()
     {
-        StartCoroutine(AddStartingPetWithDelay(3f));
-        StartCoroutine(AddStartingPetWithDelay(6f));
-        StartCoroutine(AddStartingPetWithDelay(9f));
+        for (int i = 01; i <= 10; i++)
+        {
+            StartCoroutine(AddStartingPetWithDelay(i * 3f));
+        }
     }
 
     private IEnumerator AddStartingPetWithDelay(float time)
@@ -54,13 +55,14 @@ public class PartyManager : MonoBehaviour
     public void AddToParty(GameObject pet)
     {
         // Calculate spawn position behind the last member of the party
-        Vector3 spawnDirection = party[party.Length - 1].transform.forward;
-        Debug.Log("Spawn Direction: " + spawnDirection);
-        Vector3 spawnPosition = party[party.Length - 1].transform.position + spawnDirection * 2f;
-        Debug.Log("Spawn Position: " + spawnPosition);
+        Vector3 spawnDirection = -party[party.Length - 1].transform.forward;
+        //Debug.Log("Spawn Direction: " + spawnDirection);
+        Vector3 spawnPosition = party[party.Length - 1].transform.position + spawnDirection * 1.5f;
+        //Debug.Log("Spawn Position: " + spawnPosition);
 
         // Instantiate the new pet behind the last party member
         GameObject newPet = Instantiate(pet, spawnPosition, Quaternion.identity);
+        newPet.transform.forward = transform.forward;
 
         // Update the party array to include the new pet
         GameObject[] newParty = new GameObject[party.Length + 1];
@@ -91,7 +93,7 @@ public class PartyManager : MonoBehaviour
                 // Follower pets
                 // Debug.Log("Updating Follower Pet " + i + ".");
                 playerMovement.enabled = false;
-                follow.enabled = false;  // True later.
+                follow.enabled = true;
                 follow.target = party[i - 1].transform;
             }
         }
--- Assets/Pets/Follow.cs
+++ Assets/Pets/Follow.cs
@@ -3,18 +3,8 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     public Transform target; // Reference to the target pet to follow
-    public float followDistance = 5.2f; // Distance between pets
+    public float followDistance = 2f; // Distance between pets
     public float moveSpeed = 2.0f; // Speed at which followers move towards the target
-
-    private Vector3 lastPosition; // Store the last target position
-
-    private void Start()
-    {
-        if (target != null)
-        {
-            lastPosition = target.position;
-        }
-    }
 
     private void Update()
     {
@@ -22,17 +12,15 @@ public class Follow : MonoBehaviour
         {
             Vector3 targetPosition = target.position - target.forward * followDistance;
 
-            // Calculate the movement vector
-            Vector3 moveVector = targetPosition - lastPosition;
-
             // Calculate the desired position behind the target pet
-            Vector3 desiredPosition = transform.position + moveVector;
+            Vector3 desiredPosition = targetPosition;
+            if (Vector3.Distance(transform.position, targetPosition) > followDistance)
+            {
+                desiredPosition = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            }
 
             // Move the current pet towards the desired position
-            transform.position = Vector3.Lerp(transform.position, desiredPosition, moveSpeed * Time.deltaTime);
-
-            // Store the current target position for the next frame
-            lastPosition = target.position;
+            transform.position = desiredPosition;
 
             // Rotate the pet smoothly to match the target's rotation
             Quaternion targetRotation = Quaternion.LookRotation(target.forward, Vector3.up);
