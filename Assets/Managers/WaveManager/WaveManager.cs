6d2a10009342afe0095f4b313cdec2cb1d337b58
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-08-19 10:02:36 -0500

    fix - Enemy spawn location offset

--- Assets/Managers/WaveManager/WaveManager.cs
+++ Assets/Managers/WaveManager/WaveManager.cs
@@ -38,7 +38,7 @@ public class WaveManager : MonoBehaviour
             currentWave++;
         }
 
-        //Debug.Log("All waves cleared!");
+        Debug.Log("All waves cleared!");
     }
 
     private IEnumerator SpawnWaveEnemies(int waveIndex)
@@ -46,9 +46,10 @@ public class WaveManager : MonoBehaviour
         if (waveIndex < waves.Length)
         {
             WaveConfiguration waveConfig = waves[waveIndex];
+            Vector3 waveSpawnPosition = GetRandomSpawnPosition();
             for (int i = 0; i < waveConfig.enemyCount; i++)
             {
-                Vector3 spawnPosition = GetRandomSpawnPosition();
+                Vector3 spawnPosition = GetSpawnOffset(waveSpawnPosition);
                 enemySpawner.SpawnEnemy(waveConfig.enemyTypePrefab, spawnPosition);
 
                 // Wait for the specified time before spawning the next enemy
@@ -71,4 +72,21 @@ public class WaveManager : MonoBehaviour
 
         return new Vector3(randomX, 0.125f, randomZ);
     }
+
+    private Vector3 GetSpawnOffset(Vector3 waveSpawnPosition)
+    {
+        float offsetX = Random.Range(-1f, 1f);
+        float offsetZ = Random.Range(-1f, 1f);
+
+        // Apply the offset to the wave spawn position
+        Vector3 spawnOffset = new Vector3(offsetX, 0f, offsetZ);
+        Vector3 spawnPosition = waveSpawnPosition + spawnOffset;
+
+        // Clamp the spawn position within the Clamp boundaries
+        spawnPosition.x = Mathf.Clamp(spawnPosition.x, Clamp.Instance.minX, Clamp.Instance.maxX);
+        spawnPosition.z = Mathf.Clamp(spawnPosition.z, Clamp.Instance.minZ, Clamp.Instance.maxZ);
+
+        return spawnPosition;
+    }
+
 }
