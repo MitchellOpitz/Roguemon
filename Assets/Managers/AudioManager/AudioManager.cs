e4e715231493f2b566214e4965cceaed9124dbf2
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-09-01 09:03:45 -0500

    feat - Game music

--- Assets/Managers/AudioManager/AudioManager.cs
+++ Assets/Managers/AudioManager/AudioManager.cs
@@ -1,14 +1,18 @@
 using System;
 using UnityEngine;
+using System.Collections;
+using TMPro;
 
 // To call a sound, use the following within a script:
 // FindObjectOfType<AudioManager>().PlaySound("NAME");
 public class AudioManager : MonoBehaviour
 {
-
+    public TMP_Dropdown dropdown;
     public static AudioManager instance;
     public Sound[] sounds;
-    public bool testAudio;
+
+    public int currentAudioTrackIndex; // Default track index
+    public string trackToStartPlaying;
 
     void Awake()
     {
@@ -21,7 +25,7 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
 
         // Attaches AudioSource component from each sound to the AudioManager game object.
         foreach (Sound sound in sounds)
@@ -31,12 +35,14 @@ public class AudioManager : MonoBehaviour
             sound.source.volume = sound.volume;
             sound.source.pitch = sound.pitch;
             sound.source.loop = sound.loop;
-
-            if (testAudio)
-            {
-                PlaySound(sound.name);
-            }
         }
+
+        // Load the saved audio track preference
+        currentAudioTrackIndex = PlayerPrefs.GetInt("CurrentAudioTrackIndex", currentAudioTrackIndex);
+        PlayAudioTrack(currentAudioTrackIndex);
+
+        StopAllAudio();
+        PlaySound(trackToStartPlaying);
     }
 
     public void PlaySound(string name)
@@ -48,5 +54,83 @@ public class AudioManager : MonoBehaviour
             return;
         }
         sound.source.Play();
+    }
+
+    public void PlayAudioTrack(int trackIndex)
+    {
+        if (trackIndex >= 0 && trackIndex < sounds.Length)
+        {
+            StopAllAudio();
+            currentAudioTrackIndex = trackIndex;
+            PlaySound(sounds[currentAudioTrackIndex].name);
+
+            // Save the current audio track preference
+            PlayerPrefs.SetInt("CurrentAudioTrackIndex", currentAudioTrackIndex);
+        }
+    }
+
+    private void StopAllAudio()
+    {
+        foreach (Sound sound in sounds)
+        {
+            sound.source.Stop();
+        }
+    }
+
+    public void TransitionToAudioTrack(int trackIndex)
+    {
+        StartCoroutine(FadeOutAndChangeTrack(trackIndex));
+    }
+
+    private IEnumerator FadeOutAndChangeTrack(int newTrackIndex)
+    {
+        float fadeDuration = 2.0f; // Duration of fade-out
+        float timer = 0f;
+        float startVolume = sounds[currentAudioTrackIndex].source.volume;
+
+        while (timer < fadeDuration)
+        {
+            float normalizedTime = timer / fadeDuration;
+            float currentVolume = Mathf.Lerp(startVolume, 0f, normalizedTime);
+            sounds[currentAudioTrackIndex].source.volume = currentVolume;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        sounds[currentAudioTrackIndex].source.Stop();
+        currentAudioTrackIndex = newTrackIndex;
+        PlaySound(sounds[currentAudioTrackIndex].name);
+
+        StartCoroutine(FadeInNewTrack());
+    }
+
+    private IEnumerator FadeInNewTrack()
+    {
+        float fadeDuration = 2.0f; // Duration of fade-in
+        float timer = 0f;
+        float startVolume = sounds[currentAudioTrackIndex].source.volume;
+
+        sounds[currentAudioTrackIndex].source.volume = 0f;
+        sounds[currentAudioTrackIndex].source.Play();
+
+        while (timer < fadeDuration)
+        {
+            float normalizedTime = timer / fadeDuration;
+            float currentVolume = Mathf.Lerp(0f, startVolume, normalizedTime);
+            sounds[currentAudioTrackIndex].source.volume = currentVolume;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        sounds[currentAudioTrackIndex].source.volume = startVolume;
+
+        // Update the dropdown value to reflect the selected track
+        dropdown = FindObjectOfType<TMP_Dropdown>();
+        dropdown.value = currentAudioTrackIndex;
+
+        // Save the current audio track preference
+        PlayerPrefs.SetInt("CurrentAudioTrackIndex", currentAudioTrackIndex);
     }
 }
